81835c283762442c16bd55728f2a16c806b0d1e7
david992 <24604567@qq.com>
2020-03-27 18:27:05 +0800

    flask--tamplates

---
+++ FlaskTest/run_template.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+class Person(object):
+    def say(self):
+        return " i am a person"
+
+
+@app.route("/temp")
+def temp():
+    # dic = { "title":"我的第一个模板",
+    #         "bookname":"钢铁是怎么炼成的",
+    #         "author":"奥斯特洛夫斯基",
+    #         "price":32.5,
+    #         "publisher":"北京大学出版社"}
+    # resp =  render_template("first_temp.html",values=dic)
+    title="我的第一个模板"
+    bookname="钢铁是怎么炼成的"
+    author="奥斯特洛夫斯基"
+    price=32.5
+    publisher="北京大学出版社"
+    list = ["1111","2222","3333","4444"]
+    tup = ("刘德华","张学友","郭富城","黎明")
+    dic = {
+            "1":"one",
+            "2":"two",
+            "3":"three",
+            "四":"four"}
+    per = Person()
+    per.name = "旗木卡卡西"
+    resp =  render_template("first_temp.html",values=locals())
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug=True)
